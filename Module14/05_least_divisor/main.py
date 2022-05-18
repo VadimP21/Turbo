9a92434844e83a5723df81fb444cdd4c6438bd46
Vadim Pugachev <vadimp_91@mail.ru>
2022-05-18 23:38:03 +0300

    mod14 ready to check

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def smallest_divisor(number):
+    for divider in range(2,number + 1):
+        if number % divider == 0:
+            smallest_divisor = divider
+            return smallest_divisor
+
+number = int(input('Введите число: '))
+
+smallest_divisor = smallest_divisor(number)
+print('Наименьший делитель, отличный от единицы: ', smallest_divisor)
+
--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def check_year(year):
+    year_str = str(year)
+    count = 0
+    for digit in range(0, 10):
+        for number in year_str:
+            if digit == int(number):
+                count += 1
+        if count == 3:
+            return int(year)
+        count = 0
+
+
+while True:
+    first_year = int(input('\nВведите первый год: '))
+    second_year = int(input('Введите второй год: '))
+    if first_year < 999 or first_year > 10000 or second_year < 999 or second_year > 10000:
+        print('Введите четрехзначные числа')
+    elif second_year < first_year:
+        print('Введите года по возрастанию')
+    else:
+        print('Годы от', first_year, 'до', second_year, 'с тремя одинаковыми цифрами:')
+        for year in range(first_year, second_year + 1):
+            year_special = check_year(year)
+            if year_special != None:
+                print(year_special)
